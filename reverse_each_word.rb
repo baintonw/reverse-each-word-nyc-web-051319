836369649aca97843bcc5b468d4183e464be4784
William Bainton <baintonw@gmail.com>
2019-05-04 00:22:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 hello_there = "Hello there, and how are you?"
 
 def reverse_each_word(phrase)
-    phrase.split.collect do |word|
+  phrase.split.collect do |word|
     word.reverse
   end
-   
 end
